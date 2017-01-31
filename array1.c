b16196b646d3683821d7957f18759cfa8b246d3b
Daniella Valdez <dnlvldz05@gmail.com>
2017-01-31 08:13:49 +0800

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int i, grades[3], average;
+	for(i=0; i<3; i++){ printf("Enter grade %d: ", i+1); 
+	scanf("%d", &grades[i]); average=(grades[0]+grades[1]+grades[2])/3; 
+	} 
+	printf("the average is: %d\n\n", average);
+	if( average >=75){
+	printf("Pass " );
+	}
+	else{
+    printf("Fail" );
+}
+}
